01da0c0b3b2f67277df54e46ebf77daceb864ad2
Ravi <ravinder9.reddy@gmail.com>
2020-03-30 13:05:42 +0100

    add models

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 
@@ -13,7 +13,23 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS')
         return response
 
+    @app.route('/')
+    def index():
+        return 'Hello World'
+
     @app.route('/transaction/<int:user_id>', methods=['POST'])
     def add_transaction(user_id):
         body = request.get_json()
+        description = body.get('description')
+        paid_by = body.get('paid_by')
+        amount = body.get('amount')
+        team = body.get('team')
+        split_with = body.get('split_with')
+        date_timestamp = body.get('timestamp')
+
+        return jsonify({
+            'success': 'true'
+        })
+
+    return app
 
