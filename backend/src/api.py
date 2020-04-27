16bf490169610214fa18e0e7b48795798b80291f
Ravi <ravinder9.reddy@gmail.com>
2020-04-27 04:53:49 +0100

    display lent/owed users along with balances

--- backend/src/api.py
+++ backend/src/api.py
@@ -51,6 +51,8 @@ def create_app(test_config=None):
     def display_dashboard():
         user_owed = 0
         user_lent = 0
+        user_owed_to = {}
+        user_lent_to = {}
 
         user1_balance = UserBalance.query.filter(UserBalance.user1 == 'Ravi').all()
         user2_balance = UserBalance.query.filter(UserBalance.user2 == 'Ravi').all()
@@ -58,21 +60,30 @@ def create_app(test_config=None):
         for user in user1_balance:
             if user.balance < 0:
                 user_owed += user.balance
+                user_owed_to[user.user2]: user.balance
+                # user_owed_to.append(dict(zip(user.user2, user.balance)))
 
             else:
                 user_lent += user.balance
+                user_lent_to[user.user2]: user.balance
+                # user_lent_to.append(dict(zip(user.user2, user.balance)))
         
         for user in user2_balance:
             if user.balance < 0:
                 user.balance *= (-1)
                 user_lent += user.balance
+                user_lent_to[user.user1] = user.balance
+
             else:
                 user_owed += user.balance
+                user_owed_to[user.user1] = user.balance
         
         return jsonify({
             'user': 'Ravi',
             'user_lent': user_lent,
-            'user_owed': user_owed
+            'user_owed': user_owed,
+            'user_lent_to': user_lent_to,
+            'user_owed_to': user_owed_to
 
         })
 
