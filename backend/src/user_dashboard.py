f819db44178492aac95752bd82dca08c281ace71
Ravi <ravinder9.reddy@gmail.com>
2020-04-29 06:10:15 +0100

    reformat attribute names

--- backend/src/user_dashboard.py
+++ backend/src/user_dashboard.py
@@ -6,30 +6,32 @@ class UserDashboard(object):
         pass
 
     def display_user_dashboard(self):
-        user_owed = 0
-        user_lent = 0
-        user_owed_to = {}
-        user_lent_to = {}
+        total_user_owed_amount = 0
+        total_user_lent_amount = 0
+        individual_owed_details = {}
+        individual_lent_details = {}
 
         user1_balance = UserBalance.query.filter(UserBalance.user1 == 'Ravi').all()
         user2_balance = UserBalance.query.filter(UserBalance.user2 == 'Ravi').all()
+
         for user in user1_balance:
             if user.balance < 0:
-                user_owed += user.balance
-                user_owed_to[user.user2]: user.balance
+                total_user_owed_amount += user.balance
+                individual_owed_details[user.user2]: user.balance
 
             else:
-                user_lent += user.balance
-                user_lent_to[user.user2]: user.balance
+                total_user_lent_amount += user.balance
+                individual_lent_details[user.user2]: user.balance
 
         for user in user2_balance:
             if user.balance < 0:
                 user.balance *= (-1)
-                user_lent += user.balance
-                user_lent_to[user.user1] = user.balance
+                total_user_lent_amount += user.balance
+                individual_lent_details[user.user1] = user.balance
 
             else:
-                user_owed += user.balance
-                user_owed_to[user.user1] = user.balance
+                total_user_owed_amount += user.balance
+                individual_owed_details[user.user1] = user.balance
 
-        return user_owed, user_lent, user_owed_to, user_lent_to
+        return total_user_owed_amount, total_user_lent_amount, \
+               individual_owed_details, individual_lent_details
